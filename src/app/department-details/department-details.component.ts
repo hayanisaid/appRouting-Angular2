6c52be9f2c5227f323ff47137323804f8e89a886
hayani said <hayanisaid1995@gmail.com>
2018-01-14 19:42:11 +0000

    observable aproche

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute,Router,Params} from '@angular/router';
 
 @Component({
    
@@ -8,6 +8,8 @@ import {ActivatedRoute} from '@angular/router';
        <div class="container">
        <h3>Departement Details</h3>
        <p> you navigated to Department detail id={{departId}}</p>
+       <a (click)="goPrev()">Prev</a>
+       <a (click)="goNext()">Next</a>
        </div>
     `
   
@@ -15,14 +17,30 @@ import {ActivatedRoute} from '@angular/router';
 export class DepartmentDetailsComponent implements OnInit {
     
   public departId;
-  constructor(private _route:ActivatedRoute) { }
+  constructor(private _route:ActivatedRoute,private _router:Router) { }
 
-  ngOnInit() {
-  	/* get the id param from the url*/
-  	let id=this._route.snapshot.params['id'];
+ /* ngOnInit() {
+  	// get the id param from the url
+  	// let id=this._route.snapshot.params['id'];
+  	get number id using parsInt
+  	let id=parseInt(this._route.snapshot.params['id']) ;
   	this.departId=id;
+  }*/
+
+/*usign param aprouche to work with prev and next method*/
+  ngOnInit(){
+  	this._route.params.subscribe((param:Params)=>{
+  		let id =parseInt(param['id']);
+  		this.departId=id;
+  	})
   }
 
-
-
+ goPrev(){
+ 	let prevId= this.departId -1;
+ 	this._router.navigate(['department-details/',prevId]);
+ }
+ goNext(){
+ 	let nextId=this.departId + 1;
+ 	this._router.navigate(['department-details',nextId]);
+ }
 }
